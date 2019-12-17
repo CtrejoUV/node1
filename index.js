2783f52cb0ebd2d9ed439f4e879c00d902630738
CtrejoUV <ctrejo@unitedvirtualities.com>
2019-12-17 09:55:19 -0700

    ejercice complete

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 // index.js
 
-const _ = require('lodash')
+// const fs = require('fs')
 
-_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
-// → { 'a': 1, 'b': 2, 'c': 3 }
+// console.log('start reading a file...')
+
+// fs.readFile('file.md', 'utf-8', function(err, content) {
+//     if (err) {
+//         console.log('error happened during reading the file')
+//         return console.log(err)
+//     }
+
+//     console.log(content)
+// })
+
+// console.log('end of the file')
+
+function stats(file) {
+    return new Promise((resolve, reject) => {
+        fs.stat(file, (err, data) => {
+            if (err) {
+                return reject(err)
+            }
+            resolve(data)
+        })
+    })
+}
+
+Promise.all([
+        stats('file1'),
+        stats('file2'),
+        stats('file3')
+    ])
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
